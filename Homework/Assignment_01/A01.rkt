c397a2433c6ab1eb28e6e810c1a73b00283c9f6e
irvanwang22 <irvaniac22@gmail.com>
2024-12-06 20:21:50 -0500

    add old A01 and A02

--- Homework/Assignment_01/A01.rkt
+++ Homework/Assignment_01/A01.rkt
@@ -3,32 +3,94 @@
 (provide interval-contains? interval-intersects? interval-union make-vec-from-points dot-product vector-magnitude distance)
 
 (define interval-contains?
-  (lambda (a b)
-    (nyi)))
+  (lambda (interval number)
+     (and (>= number (car interval))
+          (<= number (car (cdr interval))))
+  )
+)
 
 (define interval-intersects?
-  (lambda (a b)
-    (nyi)))
+  (lambda (i1 i2)
+    (if (< (car i1) (car i2))
+        (<= (- (car i2) (car i1)) (- (car (cdr i1)) (car i1)))
+        (<= (- (car i1) (car i2)) (- (car (cdr i2)) (car i2)))
+    )
+  )
+)
 
 (define interval-union
-  (lambda (a b)
-    (nyi)))
+  (lambda (i1 i2)
+    (if (interval-intersects? i1 i2)
+        (cons
+         (cons (get-smaller (car i1) (car i2))
+               (cons (get-greater (car (cdr i1)) (car (cdr i2))) '())) '())
+        (cons i1 (cons i2 '()))
+    )
+  )
+)
 
 (define make-vec-from-points
-  (lambda (a b)
-    (nyi)))
+  (lambda (v1 v2)
+    (if (or (null? v1) (null? v2))
+        '()
+        (cons (- (car v2) (car v1))
+              (make-vec-from-points (cdr v1) (cdr v2))
+        )
+    )
+  )
+)
 
 (define dot-product
-  (lambda (a b)
-    (nyi)))
+  (lambda (v1 v2)
+    (if (or (null? v1) (null? v2))
+        0
+        (+ (* (car v1) (car v2))
+           (dot-product (cdr v1) (cdr v2))
+        )
+    ) 
+  )
+)
 
 (define vector-magnitude
-  (lambda (a)
-    (nyi)))
+  (lambda (v)
+    (sqrt (+ (+ (square (car v))
+                (square (car (cdr v)))
+                (square (car (cdr (cdr v))))
+             )
+          )
+    )
+  )
+)
 
 (define distance
+  (lambda (p1 p2)
+    (vector-magnitude (make-vec-from-points p1 p2))
+  )
+)
+
+(define get-greater
   (lambda (a b)
-    (nyi)))
+    (if (> a b)
+        a
+        b
+    )
+  )
+)
+
+(define get-smaller
+  (lambda (a b)
+    (if (< a b)
+        a
+        b
+    )
+  )
+)
+
+(define square
+  (lambda (n)
+    (* n n)
+  )
+)
 
 ;;--------  Used by the testing mechanism   ------------------
 
--- Homework/Assignment_02/A02.rkt
+++ Homework/Assignment_02/A02.rkt
@@ -4,27 +4,66 @@
 
 (define sum-of-squares
   (lambda (a)
-    (nyi)))
+    (if (null? a)
+        0
+        (+ (* (car a) (car a))
+           (sum-of-squares (cdr a))))))
 
 (define range
   (lambda (a b)
-    (nyi)))
+    (if (>= a b)
+        '()
+        (cons a
+              (range (+ a 1) b)))))
 
 (define my-set?
   (lambda (a)
-    (nyi)))
+    (if (null? a)
+        #t
+        (if (contains? (car a) (cdr a))
+            #f
+            (my-set? (cdr a))))))
+
+(define contains?
+  (lambda (ele list)
+    (if (null? list)
+        #f
+        (if (equal? ele (car list))
+            #t
+            (contains? ele (cdr list))))))
 
 (define union
   (lambda (a b)
-    (nyi)))
+    (if (null? b)
+        a
+        (if (contains? (car b) a)
+            (union a (cdr b))
+            (cons (car b) (union a (cdr b)))))))
     
 (define more-positives?
   (lambda (lon)
-    (nyi)))
+    (> (count-positives lon)
+       (- (length lon) (count-positives lon)))))
+
+(define count-positives
+  (lambda (list)
+    (if (null? list)
+        0
+        (if (> (car list) 0)
+            (+ 1 (count-positives (cdr list)))
+            (count-positives (cdr list))))))
+
+(define length
+  (lambda (list)
+    (if (null? list)
+        0
+        (+ 1 (length (cdr list))))))
 
 (define add-quotes
   (lambda (val num)
-    (nyi)))
+    (if (<= num 0)
+        val
+        (cons 'quote (list (add-quotes val (- num 1)))))))
            
 ; Stuff for the final quine problem
 
